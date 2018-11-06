7d904cfcfa8cc798a0d5bf221005a943f4453103
Peterson Umoke <umoke10@hotmail.com>
2018-11-06 15:40:28 +0100

    updated the file to use only the classes and includes folder

--- includes/class-weed-the-types.php
+++ includes/class-weed-the-types.php
@@ -79,7 +79,6 @@ class Weed_The_Types
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -139,23 +138,6 @@ class Weed_The_Types
 
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks()
-	{
-
-		$plugin_public = new Weed_The_Types_Public($this->get_plugin_name(), $this->get_version());
-
-		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
-		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
 
 	}
 
